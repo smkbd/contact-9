acc7e51593c3a3e8c8483fea536f229e213acec4
S M Khalilullah <14842230+smkbd@users.noreply.github.com>
2023-05-10 01:47:43 +0600

    add initial files

---
+++ src/ContactServiceProvider.php
@@ -0,0 +1,16 @@
+<?php
+namespace Smkbd\Contact9;
+
+use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
+
+class ContactServiceProvider extends ServiceProvider{
+    public function register()
+    {
+        dd(45454);
+    }
+
+    public function boot()
+    {
+
+    }
+}
